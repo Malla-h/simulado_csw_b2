4c9e0e53b27b90761e1d5238563024243e307019
L.Filipe Coult <31438808+Malla-h@users.noreply.github.com>
2023-11-16 10:51:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 function adicionarPessoa() {
-    var nome = prompt("Nome:");
-    var peso = prompt("Peso:");
-    var altura = prompt("Altura:");
-
-    var imc = peso / (altura * altura);
     
+        var nome = prompt("Nome:");
+        var peso = prompt("Peso:");
+        var altura = prompt("Altura:");
 
-    var classificacao;
-    if (imc < 18.5) {
-        classificacao = "Abaixo do peso normal";
-    } else if (imc > 18.5 && imc < 25){
-        classificacao = "Peso normal";
-    } else if (imc >= 25 && im < 30) {        
-        classificacao = "Excesso de peso";    
-    } else if (imc >= 30 && im < 40) {    
-        classificacao = "Obesidade Classe I";   
-    } else if (imc >= 40) {    
-        classificacao = "Obesidade Classe III";
-    }
+        var imc = (peso / (altura * altura)).toFixed(1);
 
 
-    var table = document.getElementById("table").getElementsByTagName("tbody")[0];
-    var newRow = table.insertRow(table.rows.length);
+        var classificacao;
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso normal";
+        } else if (imc > 18.5 && imc < 25) {
+            classificacao = "Peso normal";
+        } else if (imc >= 25 && im < 30) {
+            classificacao = "Excesso de peso";
+        } else if (imc >= 30 && im < 35) {
+            classificacao = "Obesidade Classe I";
+        } else if (imc >= 35 && imc < 40) {
+            classificacao = "Obesidade Classe II";
+        } else if (imc >= 40) {
+            classificacao = "Obesidade Classe III";
+        }
 
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
 
-    cell1.innerHTML = nome;
-    cell2.innerHTML = imc;
-    cell3.innerHTML = classificacao;
+        var table = document.getElementById("table").getElementsByTagName("tbody")[0];
+        var newRow = table.insertRow(table.rows.length);
+
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+
+        cell1.innerHTML = nome;
+        cell2.innerHTML = imc;
+        cell3.innerHTML = classificacao;
     
-  }
+}
